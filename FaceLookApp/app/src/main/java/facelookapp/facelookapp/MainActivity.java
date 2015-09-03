231514971e0f08bca03dc686213783eeb71bf3e3
galor paz <galor.paz@mail.huji.ac.il>
2015-09-03 16:03:05 +0300

    ActionBarActivity

--- FaceLookApp/app/src/main/java/facelookapp/facelookapp/MainActivity.java
+++ FaceLookApp/app/src/main/java/facelookapp/facelookapp/MainActivity.java
@@ -1,17 +1,18 @@
 package facelookapp.facelookapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
     }
 
 
