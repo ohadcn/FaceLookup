fdb497f8c572e35bcce0582f25dd478eb4961f1d
Ohad Cohen <ohadcn@gmail.com>
2015-09-03 18:48:34 +0300

    create the face class

--- FaceLookApp/facedetectionlib/src/main/java/facelookapp/facedetectionlib/Face.java
+++ FaceLookApp/facedetectionlib/src/main/java/facelookapp/facedetectionlib/Face.java
@@ -1,11 +1,48 @@
 package facelookapp.facedetectionlib;
 
-import android.media.FaceDetector;
+import android.graphics.Bitmap;
 
 /**
  * Created by ohad on 9/3/15.
  */
 public class Face {
     public Face() {
+    }
+
+    /**
+     * gets you a @Face instance, from an bitmap image
+     * see also @facesFromImage
+     *
+     * @param bitmap a @bitmap image, of the face
+     * @return @Face instance
+     * @see @facesFromImage
+     */
+    public static Face faceFromBitmap(Bitmap bitmap) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * returns an array of faces in a given image.
+     *
+     * @param image the image to look in
+     * @return an array of faces
+     */
+    public static Face[] facesFromImage(Bitmap image) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * returns a number between 0 and 1 that presents the similarity of two faces
+     * 0 means the faces are totally different, 1 means they are the same
+     *
+     * @param a the first Face
+     * @param b the second Face
+     * @return a number between 0 and 1 as big as the similarity of two faces
+     */
+    public static double compareFaces(Face a, Face b) {
+        //TODO
+        return 1;
     }
 }
