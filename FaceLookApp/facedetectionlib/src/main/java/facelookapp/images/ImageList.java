ac66e2c906bcb8042e52a54c329e7f3b1aff9c7d
Ohad Cohen <ohadcn@gmail.com>
2015-09-04 05:06:53 +0300

    make filter accept string

--- FaceLookApp/facedetectionlib/src/main/java/facelookapp/images/ImageList.java
+++ FaceLookApp/facedetectionlib/src/main/java/facelookapp/images/ImageList.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.MediaStore;
 
-import java.io.File;
-import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
+
+import facelookapp.facedetectionlib.FacesStore;
 
 /**
  * Created by ohad on 9/3/15.
@@ -16,23 +16,23 @@ public class ImageList {
 
 
     public static List<String> imagesOnDevice(Context context) {
-        return imagesOnDevice(context, new FileFilter() {
+        return imagesOnDevice(context, new FacesStore.Filter() {
             @Override
-            public boolean accept(File pathname) {
+            public boolean accept(String pathname) {
                 return true;
             }
         });
     }
 
-    public static List<String> imagesOnDevice(Context context, FileFilter filter) {
+    public static List<String> imagesOnDevice(Context context, FacesStore.Filter filter) {
         //TODO filter
         List<String> results = new ArrayList<>();
 
-        String[] projections = {MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-                MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.DATE_MODIFIED
-        };
+//        String[] projections = {MediaStore.Images.Media._ID,
+//                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+//                MediaStore.Images.Media.DISPLAY_NAME,
+//                MediaStore.Images.Media.DATE_MODIFIED
+//        };
         Cursor cur = context.getContentResolver().query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 null,
@@ -48,8 +48,8 @@ public class ImageList {
 
             do {
                 data = cur.getString(dataColumn);
-                results.add(data);
-                System.out.println(data);
+                if (filter.accept(data))
+                    results.add(data);
             } while (cur.moveToNext());
         }
         return results;
